eb385b8803d6c2513b064ba12a7e45baa0fea9f4
Andressa Lopes <andressalsmenezes@gmail.com>
2023-01-30 01:44:44 -0300

    create fuctions

--- management/product_handler.py
+++ management/product_handler.py
@@ -2,12 +2,27 @@ from menu import products
 
 
 def get_product_by_id(product_id):
-    pass
+    try:
+        for product in products:
+            if product["_id"] == product_id:
+                return product
+        return {}
+    except ValueError:
+        print("O id é invalido")
 
 
 def get_products_by_type(type):
-    pass
+    try:
+        list_products = []
+        for product in products:
+            if product["type"] == type:
+                list_products.append(product)
+        return list_products
+    except ValueError:
+        print("O tipo não existe")
 
 
-def add_product(list):
-    pass
+def add_product(list, **product):
+    product["_id"] = len(list) + 1
+    list.append(product)
+    return product
